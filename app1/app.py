45716ef62845f02da0b47dfb3de4dbbeb70a8956
Rafael <ragy@gmx.com>
2023-10-13 09:55:25 +0200

    removed health endpoints, reduced imports

--- app1/app.py
+++ app1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask
 import redis
 import socket
 
@@ -17,15 +17,6 @@ def hello():
     hits = int(redis_client.get(hits_id))
     return {"message": f"Hello! I'm {server_id}", "hits": hits}
 
-@app.route('/health')
-def health_check():
-    try:
-        # Ping the Redis server
-        response = redis_client.ping()
-        if response:
-            return Response('{"status": "OK", "message": "Flask OK, Redis OK"}', content_type='application/json')
-    except Exception as e:
-        return Response('{"status": "Error", "message": "' + str(e) + '"}', content_type='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- app2/app.py
+++ app2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask
 import redis
 import socket
 import requests
@@ -24,14 +24,6 @@ def hello():
         "app1_message": app1_response.json()
     }
 
-@app.route('/health')
-def health_check():
-    try:
-        response = redis_client.ping()
-        if response:
-            return Response('{"status": "OK", "message": "Flask OK, Redis OK"}', content_type='application/json')
-    except Exception as e:
-        return Response('{"status": "Error", "message": "' + str(e) + '"}', content_type='application/json')
 
 
 if __name__ == '__main__':
